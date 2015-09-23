96f6d38bc58e61f568f2d4651173e7f009b07dbc
Jwoods1 <jwoods1@fullsail.edu>
2015-09-22 22:14:37 -0600

    added round_riders

---
+++ api/controllers/Round_ridersController.js
@@ -0,0 +1,11 @@
+/**
+ * Round_ridersController
+ *
+ * @description :: Server-side logic for managing round_riders
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Round_riders.js
@@ -0,0 +1,37 @@
+/**
+* Round_riders.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  autoCreatedAt: false,
+	autoUpdatedAt: false,
+  	attributes: {
+  		id:{
+  			type: 'integer',
+  			primaryKey: true
+  		},
+  		round_id:{
+  			type: 'integer'
+  		},
+  		rider_id:{
+  			type:'integer',
+
+  		},
+  		finished_position:{
+  			type:'integer',
+
+  		},
+  		created_at:{
+  			type:'datetime'
+  		},
+  		updated_at:{
+  			type:'datetime'
+  		}
+  		
+  }
+};
+
