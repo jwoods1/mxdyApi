ad00e904e5eb588d0e8992b231c11dd0dcf2dbfc
Jason Woods <jwoo@Jasons-Mac-Pro.local>
2015-10-06 23:30:21 -0600

    addd class by id

--- api/controllers/Race_classesController.js
+++ api/controllers/Race_classesController.js
@@ -7,7 +7,11 @@
 
 module.exports = {
 	get: function(req,res,next){
-		Race_classes.find(function(err, rclass){
+		
+		var id = req.params.id;
+		
+		if(id){
+			Race_classes.find({id: id},function(err, rclass){
 			_.forEach(rclass, function(r){
 				r.rounds = [];
 			});
@@ -32,6 +36,34 @@ module.exports = {
 			})
 			
 		})
+		}else{
+			Race_classes.find(function(err, rclass){
+			_.forEach(rclass, function(r){
+				r.rounds = [];
+			});
+			Rounds.find(function(err,rounds){
+				
+				_.forEach(rounds, function(round){
+					
+					_.forEach(rclass,function(rc){
+						
+						if(rc.id === round.race_class_id){
+							var cround = {};
+							cround.id = round.id;
+							cround.name = round.name;
+							rc.rounds.push(cround);
+							
+						}
+						
+					})
+					
+				})
+				res.json(rclass);
+			})
+			
+		})
+	
+		}
 	},
 };
 
